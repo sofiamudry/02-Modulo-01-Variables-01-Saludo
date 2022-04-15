554aa5a14fff85309e00c1d99ac72144044390e9
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-15 18:02:35 -0300

    Cambio comillas

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ alert('Hola, Bienvenide!');
 
 const nombre = prompt('Ingresa tu nombre');
 const apellido = prompt('Ingresa tu apellido');
+const edad = prompt('Ingresa tu edad');
 
-alert('Hola ${nombre} ${apellido}, bienvenida a Ada');
+alert(Hola ${apellido}, bienvenida a Ada);
