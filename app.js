d50d93f9a9d05ea2866554c4af7bb0847f886414
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-15 17:37:54 -0300

    Primer prueba

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Saludo');
+
+alert('Hola, Bienvenide!');
+
+const nombre = prompt('Ingresa tu nombre');
+const apellido = prompt('Ingresa tu apellido');
+
+alert('Hola ${nombre} ${apellido}, bienvenida a Ada');
